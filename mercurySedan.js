dec790c9df714d7e4bd1fb13cfd6204c2a28be0f
James Swinson <jswinsonwhite@gmail.com>
2023-07-08 23:30:47 -0700

    finished(?)

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,9 +1,60 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require('./vehicle').Vehicle
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    ignition() {
+        if (this.fuel > 0) {            
+            console.log("Starting engine...");
+            return this.started = true
+        } else {
+            console.log("It ain't got no gas in it");
+            return this.started = false;
+        }
+    }
+
+    passengerCount(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+
+}
+
+
+let myNewCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myNewCar.ignition()
+myNewCar.passengerCount(5)
+myNewCar.stop()
+myNewCar.checkService()
+
+console.log(myNewCar)
 
 
 //After you write the derived Car class, you should test it out.
